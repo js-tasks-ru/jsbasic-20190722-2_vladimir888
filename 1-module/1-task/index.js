ec7a7b88821483b1b7c1e49c000bae6ae8fa8f74
velsor <vl.luzan@gmail.com>
2019-07-31 08:23:16 +0300

    решение задачи - вариант 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,18 @@
  * @param {number} n index
  * @returns {number}
  */
-function pow(m, n) {
 
+function pow() {
+  const x = +prompt('Введите число.', 3);
+  const n = +prompt('Введите степень.', 3);
+  let result = 1;
+
+  if ((Math.round(x) === x) && (Math.round(n) === n)) {
+    for (let i = 0; i < n; i += 1) {
+      result *= x;
+    }
+  }
+  return alert(result);
 }
+
+pow();
