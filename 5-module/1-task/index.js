18105d0a45b0dcfdf13ceb9fda11f581d9538da7
velsor <vl.luzan@gmail.com>
2019-08-26 18:49:34 +0300

    рабочее решение, test Failed

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,30 @@
  * @param {Element} table
  */
 function highlight(table) {
+  const statusCheck = document.querySelectorAll('tbody>tr>td:last-child');
+  for (const elem of statusCheck) {
+    if (elem.dataset.available === 'true') {
+      elem.parentNode.classList.add('available');
+    } else if (elem.dataset.available === 'false') {
+      elem.parentNode.classList.add('unavailable');
+    } else if (elem.dataset) {
+      elem.parentNode.setAttribute('hidden', true);
+    }
+  }
+
+  const genderCheck = document.querySelectorAll('tbody>tr>td:nth-child(3)');
+  for (const elem of genderCheck) {
+    if (elem.innerHTML === 'm') {
+      elem.parentNode.classList.add('male');
+    } else {
+      elem.parentNode.classList.add('female');
+    }
+  }
+
+  const ageCheck = document.querySelectorAll('tbody>tr>td:nth-child(2)');
+  for (const elem of ageCheck) {
+    if (elem.innerHTML < 18) {
+      elem.parentNode.style.textDecoration = 'line-through';
+    }
+  }
 }
